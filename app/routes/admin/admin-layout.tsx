47cd5813b8c911c9f60c11141d14e9f86dd804e1
Marcelo Melogno <marcelomelogno1986@gmail.com>
2025-08-06 22:01:39 -0300

    Implement full authentication

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,19 +1,42 @@
-import { Outlet } from "react-router"
-import { SidebarComponent } from '@syncfusion/ej2-react-navigations'
-import { MobileSidebar, NavItems } from "../../../components/Index"
+import {Outlet, redirect, useLoaderData} from "react-router"
+import {SidebarComponent} from '@syncfusion/ej2-react-navigations'
+import {MobileSidebar, NavItems} from "../../../components/Index"
+import {account} from "~/appwrite/client";
+import {getExistingUser, storeUserData} from "~/appwrite/auth";
+
+export async function clientLoader() {
+    try {
+        const user = await account.get()
+        if (!user.$id) return redirect('/sign-in')
+
+        const existingUser = await getExistingUser(user.$id)
+        console.log('existingUser: ', existingUser)
+        if (existingUser?.status === 'user') {
+            return redirect('/')
+        }
+
+        return existingUser?.$id ? existingUser : await storeUserData()
+    } catch (err) {
+        console.log('clientLoader error: ', err)
+        return redirect('/sign-in')
+    }
+}
+
+
 const AdminLayout = () => {
+
     return (
         <div className="admin-layout">
-            <MobileSidebar />
+            <MobileSidebar/>
 
             <aside className="w-full max-w-[270px] hidden lg:block">
                 <SidebarComponent width={270} enableGestures={false}>
-                    <NavItems />
+                    <NavItems/>
                 </SidebarComponent>
             </aside>
 
             <aside className="children">
-                <Outlet />
+                <Outlet/>
             </aside>
         </div>
     )
--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,27 +1,35 @@
-import { cn } from "lib/util"
-import { Link, NavLink } from "react-router"
-import { sidebarItems } from "~/constants"
+import {cn} from "lib/util"
+import {Link, NavLink, useLoaderData, useNavigate} from "react-router"
+import {sidebarItems} from "~/constants"
+import {logoutUser} from "~/appwrite/auth";
 
-const NavItems = ({handleClick}:{handleClick? : () => void}) => {
-    const user = {
-        name: 'Marcelo',
-        email: 'contact@mmdeveloper.pro',
-        imageUrl: '/assets/images/david.webp',
+const NavItems = ({handleClick}: {
+    handleClick?: () => void,
+}) => {
+
+    const userData = useLoaderData() as { name?: string; email?: string; imageUrl?: string } | null
+    const name = userData?.name || 'User'
+    const email = userData?.email || "user@user.com"
+    const imageUrl = userData?.imageUrl || '/assets/images/david.webp'
+
+    const navigate = useNavigate()
+    const handleLogout = async () => {
+        await logoutUser()
+        navigate('/sign-in')
     }
-    const handleLogout = ()=>{}
 
     return (
         <section className="nav-items">
             <Link to='/' className="link-logo">
-                <img src="/assets/icons/logo.svg" alt="logo" className="size-[30px]" />
+                <img src="/assets/icons/logo.svg" alt="logo" className="size-[30px]"/>
                 <h1>Tourvisto</h1>
             </Link>
 
             <div className="container">
                 <nav>
-                    {sidebarItems.map(({ id, href, icon, label }) => (
+                    {sidebarItems.map(({id, href, icon, label}) => (
                         <NavLink to={href} key={id}>
-                            {({ isActive }: { isActive: boolean }) => (
+                            {({isActive}: { isActive: boolean }) => (
                                 <div className={cn('group nav-item', {
                                     'bg-primary-100 !text-white': isActive
                                 })} onClick={handleClick}>
@@ -38,11 +46,12 @@ const NavItems = ({handleClick}:{handleClick? : () => void}) => {
                 </nav>
 
                 <footer className="nav-footer">
-                    <img src={user?.imageUrl || '/assets/images/david.webp'} alt={user?.name || 'David'} referrerPolicy="no-referrer" />
+                    <img src={imageUrl} alt={name || 'David'}
+                         referrerPolicy="no-referrer"/>
 
                     <article>
-                        <h2>{user?.name}</h2>
-                        <p>{user?.email}</p>
+                        <h2>{name}</h2>
+                        <p>{email}</p>
                     </article>
 
                     <button
